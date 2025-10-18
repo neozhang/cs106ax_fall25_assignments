4d93f2ee3a76dd84266be5662554facd5d7a0fb3
Neo Zhang <neo.zhangning@gmail.com>
2025-10-18 16:32:22 -0700

    [assign 4] mileston 4: connect the keyboard and lamp panel

--- 4/code/Enigma.js
+++ 4/code/Enigma.js
@@ -65,6 +65,7 @@ function runEnigmaSimulation(gw) {
 
 class Keyboard {
   constructor(enigma, gw, clickable) {
+    this.enigma = enigma;
     this.gw = gw;
     this.style = clickable ? KEY_STYLE : LAMP_STYLE;
     const letters = enigma.ALPHABET;
@@ -81,9 +82,9 @@ class Keyboard {
         ],
         element: this.createKey(
           letters[i],
-          this.style.colorMap[0],
           this.style.locations[i]["x"] - this.style.radius,
           this.style.locations[i]["y"] - this.style.radius,
+          this.style.colorMap[0],
         ),
       };
       keys.push(key);
@@ -94,27 +95,42 @@ class Keyboard {
     if (clickable) this.attachListeners();
   }
 
-  createKey(letter, keyColor, x, y) {
+  createKey(
+    letter,
+    x,
+    y,
+    keyColor,
+    radius,
+    border,
+    borderColor,
+    bgColor,
+    labelDy,
+    font,
+  ) {
     const compound = GCompound(x, y);
+
+    radius = radius ? radius : this.style.radius;
+    border = border ? border : this.style.border;
+    borderColor = borderColor ? borderColor : this.style.borderColor;
+    bgColor = bgColor ? bgColor : this.style.bgColor;
+    labelDy = labelDy ? labelDy : this.style.labelDy;
+    font = font ? font : this.style.font;
+
     const innerOval = GOval(
-      this.style.border,
-      this.style.border,
-      (this.style.radius - this.style.border) * 2,
-      (this.style.radius - this.style.border) * 2,
+      border,
+      border,
+      (radius - border) * 2,
+      (radius - border) * 2,
     );
-    const outerOval = GOval(this.style.radius * 2, this.style.radius * 2);
+    const outerOval = GOval(radius * 2, radius * 2);
     innerOval.setFilled(true);
-    innerOval.setColor(this.style.bgColor);
+    innerOval.setColor(bgColor);
     outerOval.setFilled(true);
-    outerOval.setColor(this.style.borderColor);
+    outerOval.setColor(borderColor);
 
     letter = letter.charAt(0).toUpperCase();
-    const label = GLabel(
-      letter,
-      this.style.radius,
-      this.style.radius + this.style.labelDy,
-    );
-    label.setFont(this.style.font);
+    const label = GLabel(letter, radius, radius + labelDy);
+    label.setFont(font);
     label.setColor(keyColor);
     label.setTextAlign("center");
 
@@ -129,7 +145,10 @@ class Keyboard {
     const handleClick = (e) => {
       const obj = this.gw.getElementAt(e.getX(), e.getY());
       for (const key of this.keys) {
-        if (key.element === obj) this.toggleKeyColor(key);
+        if (key.element === obj) {
+          this.toggleKeyColor(key);
+          this.toggleLampColor(key);
+        }
       }
     };
     this.gw.addEventListener("mousedown", handleClick);
@@ -144,14 +163,39 @@ class Keyboard {
         : this.style.colorMap[0];
     const newKeyElement = this.createKey(
       key.letter,
-      newColor,
       key.position[0],
       key.position[1],
+      newColor,
     );
     key.element = newKeyElement;
     key.color = newColor;
     this.keys[key.index] = key;
     this.gw.add(newKeyElement);
+  }
+
+  toggleLampColor(key) {
+    const lamp = this.enigma.lamps[key.index];
+    this.gw.remove(lamp.element);
+    const newColor =
+      lamp.color === LAMP_STYLE.colorMap[0]
+        ? LAMP_STYLE.colorMap[1]
+        : LAMP_STYLE.colorMap[0];
+    const newLampElement = this.createKey(
+      lamp.letter,
+      lamp.position[0],
+      lamp.position[1],
+      newColor,
+      LAMP_STYLE.radius,
+      LAMP_STYLE.border,
+      LAMP_STYLE.borderColor,
+      LAMP_STYLE.bgColor,
+      LAMP_STYLE.labelDy,
+      LAMP_STYLE.font,
+    );
+    lamp.element = newLampElement;
+    lamp.color = newColor;
+    this.enigma.lamps[key.index] = lamp;
+    this.gw.add(newLampElement);
   }
 }
 
