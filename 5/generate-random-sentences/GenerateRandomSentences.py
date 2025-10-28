7fbf664bd410f70679e00b6aeedc84b999e393f5
Neo Zhang <neo.zhangning@gmail.com>
2025-10-27 22:55:59 -0700

    [assign 5] feat: implement readGrammar parser and helpers

--- 5/generate-random-sentences/GenerateRandomSentences.py
+++ 5/generate-random-sentences/GenerateRandomSentences.py
@@ -3,15 +3,57 @@
 # This file exports a program that reads in a grammar file and
 # then prints three randomly generated sentences
 
+from webbrowser import get
 from filechooser import chooseInputFile
 from random import choice
 
 
 def readGrammar(filename):
-    return {}  # replace this with your own implementation
+    with open(filename) as file:
+        lines = file.read().splitlines()
+        result = {}
+        nonterm = ""
+        for line in lines:
+            line = line.strip()
+            if line[0] == "<" and len(getNonterminalIndex(line)) == 1:
+                nonterm = line[1:-1]  # Remove the brackets
+                continue
+            elif isInteger(line):
+                length_of_expansions = int(line)
+                curr = 0
+                expansions = []
+                continue
+            elif curr < length_of_expansions:
+                expansions.append(line)
+                curr += 1
+                continue
+            result[nonterm] = expansions
+    return result
+
+
+def getNonterminalIndex(line):
+    result = []
+    while line:
+        try:
+            startIdx = line.index("<")
+            endIdx = line.index(">")
+            result.append([startIdx, endIdx])
+            line = line[endIdx + 1 :]
+        except ValueError:
+            break
+    return result
+
+
+def isInteger(line):
+    try:
+        int(line)
+        return True
+    except ValueError:
+        return False
 
 
 def generateRandomSentence(grammar):
+    print(grammar)
     return "[placeholder for a random sentence]"  # replace this with your own implementation
 
 
