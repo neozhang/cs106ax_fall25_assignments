38c05a39a78f8f4441debebd3b88ecc4caa78ea8
Neo Zhang <neo.zhangning@gmail.com>
2025-11-13 21:20:31 -0800

    [assign 7] [m1] feat: add initFlags to generate shuffled flag list

--- 7/code/match-the-flags.js
+++ 7/code/match-the-flags.js
@@ -6,22 +6,32 @@
 "use strict";
 
 function BootstrapMatchTheFlag() {
+  /*
+   * Function: shuffle
+   * -----------------
+   * Generically shuffles the supplied array so
+   * that any single permutation of the elements
+   * is equally likely.
+   */
+  function shuffle(array) {
+    for (let lh = 0; lh < array.length; lh++) {
+      let rh = lh + Math.floor(Math.random() * (array.length - lh));
+      let temp = array[rh];
+      array[rh] = array[lh];
+      array[lh] = temp;
+    }
+  }
 
-   /*
-    * Function: shuffle
-    * -----------------
-    * Generically shuffles the supplied array so
-    * that any single permutation of the elements
-    * is equally likely.
-    */
-   function shuffle(array) {
-      for (let lh = 0; lh < array.length; lh++) {
-         let rh = lh + Math.floor(Math.random() * (array.length - lh));
-         let temp = array[rh];
-         array[rh] = array[lh];
-         array[lh] = temp;
-      }    
-   }
+  function initFlags() {
+    let flags = [];
+    const IMGPATH = "./images/";
+    for (const country of COUNTRIES) {
+      flags.push(IMGPATH + country.toLowerCase() + ".png");
+      flags.push(IMGPATH + country.toLowerCase() + ".png");
+    }
+    shuffle(flags);
+    return flags;
+  }
 }
 
 /* Execute the above function when the DOM tree is fully loaded. */
