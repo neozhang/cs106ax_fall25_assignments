0f04934902c285738f7179447d385e9f408beb51
Neo Zhang <neo.zhangning@gmail.com>
2025-11-21 12:14:59 -0800

    [assign 8] api: implement get_floots and get_floot handlers

--- 8/code/server/api.py
+++ 8/code/server/api.py
@@ -12,6 +12,7 @@ CLIENT_SRC_DIR = os.path.abspath(os.path.join(SERVER_SRC_DIR, "..", "client"))
 
 db = Database()
 
+
 # GET /
 def serve_file(path):
     """
@@ -24,14 +25,18 @@ def serve_file(path):
         path = "/index.html"
     target_file_path = os.path.abspath(os.path.join(CLIENT_SRC_DIR, path[1:]))
     # Avoid serving files above client source directory for security
-    if not target_file_path.startswith(CLIENT_SRC_DIR + os.sep) \
-            or not os.path.isfile(target_file_path):
+    if not target_file_path.startswith(CLIENT_SRC_DIR + os.sep) or not os.path.isfile(
+        target_file_path
+    ):
         return HTTPError(404, "File not found")
 
     with open(target_file_path, "rb") as f:
         # Return the file. Guess the content-type based on the file extension
         # (e.g. a .html file is probably text/html).
-        return Response(f.read(), content_type=mimetypes.guess_type(target_file_path)[0])
+        return Response(
+            f.read(), content_type=mimetypes.guess_type(target_file_path)[0]
+        )
+
 
 # GET /api/floots
 def get_floots():
@@ -43,8 +48,12 @@ def get_floots():
     not straightforward to send an arbitrary object, like a Floot object, over
     the internet.) You may find the Floot to_dictionary() method helpful.
     """
-    # TODO: delete the following line, and replace it with your own implementation
-    return HTTPError(501, "api.get_floots not implemented yet")
+    floots = db.get_floots()
+    res = []
+    for floot in floots:
+        res.append(floot.to_dictionary())
+    return res
+
 
 # GET /api/floots/{floot_id}
 def get_floot(floot_id):
@@ -54,8 +63,11 @@ def get_floot(floot_id):
     You should return the floot as a dictionary, not as a Floot object (see
     Floot.to_dictionary()).
     """
-    # TODO: delete the following line, and replace it with your own implementation
-    return HTTPError(501, "api.get_floot not implemented yet")
+    if db.has_floot(floot_id):
+        return db.get_floot_by_id(floot_id).to_dictionary()
+    else:
+        return HTTPError(404, "Provided floot ID could not be found")
+
 
 # POST /api/floots
 def create_floot(request_body):
@@ -78,6 +90,7 @@ def create_floot(request_body):
     """
     # TODO: delete the following line, and replace it with your own implementation
     return HTTPError(501, "api.create_floot not implemented yet")
+
 
 # POST /api/floots/{floot_id}/delete
 def delete_floot(floot_id, request_body):
@@ -102,6 +115,7 @@ def delete_floot(floot_id, request_body):
     # TODO: delete the following line, and replace it with your own implementation
     return HTTPError(501, "api.delete_floot not implemented yet")
 
+
 # GET /api/floot/{floot_id}/comments
 def get_comments(floot_id):
     """
@@ -111,6 +125,7 @@ def get_comments(floot_id):
     """
     # TODO: delete the following line, and replace it with your own implementation
     return HTTPError(501, "api.get_comments not implemented yet")
+
 
 # POST /api/floots/{floot_id}/comments
 def create_comment(floot_id, request_body):
@@ -127,6 +142,7 @@ def create_comment(floot_id, request_body):
     """
     # TODO: delete the following line, and replace it with your own implementation
     return HTTPError(501, "api.create_comment not implemented yet")
+
 
 # POST /api/floots/{floot_id}/comments/{comment_id}/delete
 def delete_comment(floot_id, comment_id, request_body):
@@ -151,6 +167,7 @@ def delete_comment(floot_id, comment_id, request_body):
     # TODO: delete the following line, and replace it with your own implementation
     return HTTPError(501, "api.delete_comment not implemented yet")
 
+
 # POST /api/floots/{floot_id}/like
 def like_floot(floot_id, request_body):
     """
@@ -171,6 +188,7 @@ def like_floot(floot_id, request_body):
     # TODO: if you're trying to do this extension, delete the following line,
     # and replace it with your own implementation
     return HTTPError(501, "api.like_floot not implemented yet")
+
 
 # POST /api/floots/{floot_id}/unlike
 def unlike_floot(floot_id, request_body):
@@ -193,6 +211,7 @@ def unlike_floot(floot_id, request_body):
     # and replace it with your own implementation
     return HTTPError(501, "api.unlike_floot not implemented yet")
 
+
 # This specifies which functions should be called given a particular incoming
 # path. You don't need to understand or change this, unless you're doing an
 # extension that requires adding new API routes.
@@ -209,8 +228,11 @@ GET_ROUTES = [
 POST_ROUTES = [
     (("/api/floots"), create_floot),
     (("/api/floots/(.*?)/comments", "floot_id"), create_comment),
-    (("/api/floots/(.*?)/comments/(.*)/delete", "floot_id", "comment_id"), delete_comment),
+    (
+        ("/api/floots/(.*?)/comments/(.*)/delete", "floot_id", "comment_id"),
+        delete_comment,
+    ),
     (("/api/floots/(.*)/delete", "floot_id"), delete_floot),
     (("/api/floots/(.*)/like", "floot_id"), like_floot),
-    (("/api/floots/(.*)/unlike", "floot_id"), unlike_floot)
+    (("/api/floots/(.*)/unlike", "floot_id"), unlike_floot),
 ]
